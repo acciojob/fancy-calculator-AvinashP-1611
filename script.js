761016422ae70bb69f2bec8faf98245831376dc7
Avinash <134164909+AvinashP-1611@users.noreply.github.com>
2024-01-07 18:45:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function cal(val1,val2){
+	const v=parseInt(val1);
+	return parseInt(val2)+(v*2);
+}
+const val1=prompt("Enter first input");
+const val2=prompt("Enter second input");
+	alert(cal(val1,val2));
